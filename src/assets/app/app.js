4241f90be4103f39cc095864743db1e4e7b85e84
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-05-16 18:03:41 +0200

    menu hamburguesa mobile añadido

--- src/assets/app/app.js
+++ src/assets/app/app.js
@@ -1,5 +1,21 @@
-const noticia = document.querySelectorAll("li")[3];
+const hamb = document.querySelector("#hamb");
+const hamb_link = document.querySelectorAll("ul.side li");
+const body = document.body;
+const sideNav = document.querySelector(".side")
 
-noticia.addEventListener("click", () => {
-    alert("Ves a preparar palomitas para cuando hagas click en el vídeo :)")
-})
+
+hamb.addEventListener("click", function(event) {
+    sideNav.classList.toggle("active");
+    hamb.style.display = "none";
+    event.stopPropagation(); // Evita que el clic se propague al body
+});
+
+body.addEventListener("click", function() {
+    if (checkMediaQuery() && sideNav.classList.contains("active")) {
+        sideNav.classList.remove("active");
+        hamb.style.display = "contents";
+    }
+});
+
+
+
